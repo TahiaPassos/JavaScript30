b99b34d7918ca38e96fcc9e54e6afdaa01a3b9f6
Tahiana Passos <tahianasp@protonmail.com>
2021-01-29 18:49:26 -0300

    Fotos do código

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -2,12 +2,17 @@ const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+//Função que será acionada a cada segundo através do setInterval
 function setDate() {
+
+  //Cria-se um objeto do tipo Date e usa-se seus métodos para conseguir os segundos, minutos e horas atuais
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
 
+  //Condicional que desativa a propriedade transition no CSS quando os valores chegarem em 0
+  //para corrigir um bug da estilização
   if(seconds === 0 || minutes === 0 || hours === 0){
     secondHand.classList.add('nontransition');
     minHand.classList.add('nontransition');
@@ -18,6 +23,7 @@ function setDate() {
     hourHand.classList.remove('nontransition');
   }
 
+  //Conversão do segundos, minutos e horas em graus para posicionar os ponteiros
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   const hoursDegrees = ((hours / 12) * 360) + 90;
@@ -25,7 +31,7 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
 }
 
+//Ativa a função setDate a cada segundo
 setInterval(setDate, 1000);
